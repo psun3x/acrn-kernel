d829446136e680577635c5fb818dd2da0a75ec52
Ping Gao <ping.a.gao@intel.com>
2017-09-06 19:57:57 +0800

    drm/i915/gvt: Add a module parameter to disable conformance check
    By default the guest conformance check is enabled.
    
    Signed-off-by: Ping Gao <ping.a.gao@intel.com>
    Reviewed-by: Kevin Tian
    Reviewed-by: Singh, Satyeshwar <satyeshwar.singh@intel.com>
    
    Conflicts:
    	drivers/gpu/drm/i915/i915_params.c
    	drivers/gpu/drm/i915/i915_params.h
    
    Reviewed-on:
    Reviewed-by: He, Min <min.he@intel.com>
    Reviewed-by: Jiang, Fei <fei.jiang@intel.com>
    Reviewed-by: Dong, Eddie <eddie.dong@intel.com>
    Tested-by: Dong, Eddie <eddie.dong@intel.com>
    
    V2: rebase 4.19
    Signed-off-by: Xinyun Liu <xinyun.liu@intel.com>

--- drivers/gpu/drm/i915/i915_params.h
+++ drivers/gpu/drm/i915/i915_params.h
@@ -80,8 +80,10 @@ struct drm_printer;
 	param(int, domain_scaler_owner, 0x11100) \
 	param(unsigned int, enable_pvmmio, \
 			PVMMIO_ELSP_SUBMIT | PVMMIO_PLANE_UPDATE \
-			| PVMMIO_PLANE_WM_UPDATE | PVMMIO_PPGTT_UPDATE) \
-	param(bool, enable_gvt, false)
+			| PVMMIO_PLANE_WM_UPDATE | PVMMIO_PPGTT_UPDATE \
+			| PVMMIO_GGTT_UPDATE ) \
+	param(bool, enable_gvt, false) \
+	param(bool, enable_conformance_check, true)
 
 #define MEMBER(T, member, ...) T member;
 struct i915_params {
