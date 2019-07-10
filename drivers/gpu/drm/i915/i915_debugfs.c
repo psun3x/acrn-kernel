b54521d27a9d7636a45f6bab530e3bdae6e5d455
Min He <min.he@intel.com>
2019-07-10 06:59:18 +0000

    drm/i915: add GEN9 cache sharing control in debugfs
    To support cache sharing QoS, we added support for Gen9 in this patch.
    
    Tracked-On: projectacrn/acrn-hypervisor#3392
    Signed-off-by: Min He <min.he@intel.com>
    Reviewed-by: Zhao Yakui <yakui.zhao@intel.com>

--- drivers/gpu/drm/i915/i915_debugfs.c
+++ drivers/gpu/drm/i915/i915_debugfs.c
@@ -3965,7 +3965,8 @@ i915_cache_sharing_get(void *data, u64 *val)
 	intel_wakeref_t wakeref;
 	u32 snpcr = 0;
 
-	if (!(IS_GEN_RANGE(dev_priv, 6, 7)))
+	if (!(IS_GEN(dev_priv, 6) || IS_GEN(dev_priv, 7)
+				|| IS_GEN(dev_priv, 9)))
 		return -ENODEV;
 
 	with_intel_runtime_pm(dev_priv, wakeref)
@@ -3981,8 +3982,10 @@ i915_cache_sharing_set(void *data, u64 val)
 {
 	struct drm_i915_private *dev_priv = data;
 	intel_wakeref_t wakeref;
+	u32 snpcr, idicr;
 
-	if (!(IS_GEN_RANGE(dev_priv, 6, 7)))
+	if (!(IS_GEN(dev_priv, 6) || IS_GEN(dev_priv, 7)
+				|| IS_GEN(dev_priv, 9)))
 		return -ENODEV;
 
 	if (val > 3)
@@ -3997,6 +4000,13 @@ i915_cache_sharing_set(void *data, u64 val)
 		snpcr &= ~GEN6_MBC_SNPCR_MASK;
 		snpcr |= val << GEN6_MBC_SNPCR_SHIFT;
 		I915_WRITE(GEN6_MBCUNIT_SNPCR, snpcr);
+	}
+
+	if (IS_GEN(dev_priv, 9)) {
+		idicr = I915_READ(HSW_IDICR);
+		idicr &= ~IDI_QOS_MASK;
+		idicr |= (val << IDI_QOS_SHIFT);
+		I915_WRITE(HSW_IDICR, idicr);
 	}
 
 	return 0;
